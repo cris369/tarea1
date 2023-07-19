38e6afe0c82225a1a46e22f5b424ecc0ba93dd8f
cris369 <echeverriacristian1975@gmail.com>
2023-07-19 19:30:57 -0300

    subiendo primer archivo

---
+++ repositorio.py
@@ -0,0 +1,21 @@
+nombre = "Cristian"
+apellido = "Echeverria"
+edad = 47
+altura = 1.84
+peso = 111
+
+print(nombre, apellido)
+print(edad)
+print(altura, peso)
+
+
+
+number1 = 3
+number2 = 6
+number3 = 9
+
+number1 = (number1 + 10) * 5
+number2 = number2 - 5 + number1
+number3 = (number3 + number2) / 4
+
+print(number3)
